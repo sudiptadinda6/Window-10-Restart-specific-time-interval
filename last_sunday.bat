39f57fb7b96406b72f41ebe2f48a83018b8b72a1
unknown <sudiptadinda6@gmail.com>
2023-07-18 13:03:02 +0530

    update files

--- last_sunday.bat
+++ last_sunday.bat
@@ -3,23 +3,19 @@ setlocal
 
 rem Get the current date and time in PowerShell and calculate the Sunday date
 for /f "delims=" %%a in ('powershell -Command "(Get-Date -Day 1).addMonths(1).addDays(-1).addDays(-(Get-Date -Day 1).addMonths(1).addDays(-1).dayOfWeek.value__).ToString('dd')"') do (
-    set "sundayDate=13"
+    set "lastSunday=%%a"
 )
+
+rem Get todays date
 for /f "delims=" %%a in ('powershell -Command "(Get-Date).ToString('dd')"') do (
-    set "Date=%%a"
+    set "today=%%a"
 )
 
-echo %sundayDate%
-echo %Date%
-
-IF "%sundayDate%"=="%Date%" (
-    shutdown /r /t 0
-    echo The day number of the last Sunday of the current month is: %sundayDate%
-     
-) ELSE (
-    echo Opps !!!! not sunday : %Date%
+if "%lastSunday%"=="%today%" (
+    echo Today is the last sunday of the month. Hence, restarting the server.
+    shutdown /r /t 0     
+) else (
+    echo Today is not last last sunday of the month. Hence, no action taken.
 )
-
-
 
 endlocal
